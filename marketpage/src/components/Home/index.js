851b927d393266c2f618c0d0da4a4370bcc48bbc
Jake Krueger <jmk4u14@gmail.com>
2021-03-03 22:46:56 -0500

    Did CSS stylings to navbar and created drop down. Started working on infrastructure for homepage

--- marketpage/src/components/Home/index.js
+++ marketpage/src/components/Home/index.js
@@ -1,5 +1,25 @@
-export default function HomePage() {
+import React from 'react'
+import { Container, Row, Col } from "reactstrap"
+
+const HomePage = () => {
     return (
-    <h1>Welcome to the home screen!</h1>
+    <Container>
+        <Row>
+            <Col xs="8" className="main-post">1 of 2</Col>
+            <Col>
+                <Row className="side-post">1 of 3</Row>
+                <Row className="side-post">2 of 3</Row>
+                <Row className="side-post">3 of 3</Row>
+            </Col>
+        </Row>
+        <br></br>
+        <Row>
+            <Col>1 of 3</Col>
+            <Col>2 of 3</Col>
+            <Col>3 of 3</Col>
+        </Row>
+    </Container>
     );
 }
+
+export default HomePage
