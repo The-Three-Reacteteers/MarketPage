9bdc62081e3ea3e02ebaa7e35d544792b542542c
Jake Krueger <jmk4u14@gmail.com>
2021-03-04 21:07:58 -0500

    Uploaded new logos, finished up navbar css, put background logo on signup and login page

--- marketpage/src/components/SignUp/index.js
+++ marketpage/src/components/SignUp/index.js
@@ -1,5 +1,7 @@
+import Background from "../background";
+
 export default function SignUpPage() {
     return (
-    <h1>Welcome to the sign-up screen!</h1>
+    <Background />
     );
 }
---
+++ marketpage/src/components/background.js
@@ -0,0 +1,7 @@
+import BackgroundImage from "../assets/images/MarketPage.png";
+
+export default function Background() {
+    return (
+    <img src={BackgroundImage} className="background"/>
+    );
+}
