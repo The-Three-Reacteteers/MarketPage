2fd795767e79bb0d71dad0180a02e842070a9052
Jake Krueger <jmk4u14@gmail.com>
2021-03-04 21:39:01 -0500

    Added non-working forms and css to log in and sign up pages

--- marketpage/src/components/LogIn/index.js
+++ marketpage/src/components/LogIn/index.js
@@ -1,7 +1,31 @@
 import Background from "../background";
+import React, { Component } from "react";
+import { Container } from "reactstrap"
 
-export default function LogInPage() {
-    return (
-    <Background />
-    );
+export default class LoginInPage extends Component {
+    render() {
+        return (
+            <Container className="login">
+            <form>
+                <h3 className="center-text">Log in</h3>
+                <div className="form-group">
+                    <label>Email</label>
+                    <input type="email" className="form-control" placeholder="Enter email" />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
+                <div className="form-group">
+                    <div className="custom-control custom-checkbox">
+                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                    </div>
+                </div>
+                <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in</button>
+            </form>
+            </Container>
+        ); 
+    }
 }
