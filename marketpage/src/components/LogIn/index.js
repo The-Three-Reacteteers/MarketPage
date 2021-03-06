d66bb8e9d89fddfecbf6da03dbd5e97b882cec9c
Jake Krueger <jmk4u14@gmail.com>
2021-03-06 11:10:30 -0500

    Added background image to log in and sign up pages. Modified background image css

--- marketpage/src/components/LogIn/index.js
+++ marketpage/src/components/LogIn/index.js
@@ -6,6 +6,7 @@ export default class LoginInPage extends Component {
     render() {
         return (
             <Container className="login">
+                <Background />
             <form>
                 <h3 className="center-text">Log in</h3>
                 <div className="form-group">
@@ -17,15 +18,9 @@ export default class LoginInPage extends Component {
                     <label>Password</label>
                     <input type="password" className="form-control" placeholder="Enter password" />
                 </div>
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                    </div>
-                </div>
                 <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in</button>
             </form>
             </Container>
-        ); 
+        );
     }
 }
