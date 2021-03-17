4824138fa41fba0833e5eaa329300829f4426af7
skalback <70301513+skalback@users.noreply.github.com>
2021-03-16 22:09:51 -0400

    updates to routes

--- marketpage/controllers/userController.js
+++ marketpage/controllers/userController.js
@@ -1,1 +1,16 @@
 const db = require('../models');
+
+//Define methods for userController
+
+module.exports = {
+   // write new user to db  
+   create: function(req, res) {
+      //console.log(req.body)
+
+   }
+
+   
+   
+
+
+}
