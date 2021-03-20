6d0874f14aad0a36f3d87e62a57ee17960204dd6
Jake Krueger <jmk4u14@gmail.com>
2021-03-19 21:25:41 -0400

    Updated Logo sizing

--- marketpage/client/src/components/Home/Homepage.js
+++ marketpage/client/src/components/Home/Homepage.js
@@ -14,48 +14,32 @@ const Homepage = () => {
             <Row >
                 <img src={Logo} className="logo"/>
             </Row>
-        <br></br>
+        <br></br><br></br><br></br>
         <Row><Col><h1 className="mt-4 mb-4 text-white genre-fz" >Books to Enjoy</h1></Col></Row>
     <CardDeck className="ml-5 mr-5">
         <Card className="border-0 z-index" >
     <Link><Card.Img variant="top" src="https://cdn.wallpapersafari.com/3/57/r2K0SO.jpg"/></Link>
-        <Card.Body className="bkg-blk">
-        </Card.Body>
     </Card>
     <Card className="border-0 z-index" >
     <Link><Card.Img variant="top" src="https://cdn.wallpapersafari.com/3/57/r2K0SO.jpg"/></Link>
-        <Card.Body className="bkg-blk">
-        </Card.Body>
-    </Card>
-    <Card className="hide-xs border-0 z-index" >
-    <Link><Card.Img variant="top" src="https://cdn.wallpapersafari.com/3/57/r2K0SO.jpg"/></Link>
-        <Card.Body className="bkg-blk">
-        </Card.Body>
     </Card>
     <Card className="hide-sm border-0 z-index" >
     <Link><Card.Img variant="top" src="https://cdn.wallpapersafari.com/3/57/r2K0SO.jpg"/></Link>
-        <Card.Body className="bkg-blk">
-        </Card.Body>
     </Card>
     <Card className="hide-sm border-0 z-index" >
     <Link><Card.Img variant="top" src="https://cdn.wallpapersafari.com/3/57/r2K0SO.jpg"/></Link>
-        <Card.Body className="bkg-blk">
-        </Card.Body>
+    </Card>
+    <Card className="hide-sm border-0 z-index" >
+    <Link><Card.Img variant="top" src="https://cdn.wallpapersafari.com/3/57/r2K0SO.jpg"/></Link>
     </Card>
     <Card className="hide-md border-0 z-index" >
     <Link><Card.Img variant="top" src="https://cdn.wallpapersafari.com/3/57/r2K0SO.jpg"/></Link>
-        <Card.Body className="bkg-blk">
-        </Card.Body>
     </Card>
     <Card className="hide-lg border-0 z-index" >
     <Link><Card.Img variant="top" src="https://cdn.wallpapersafari.com/3/57/r2K0SO.jpg"/></Link>
-        <Card.Body className="bkg-blk">
-        </Card.Body>
     </Card>
     <Card className="hide-xl border-0 z-index" >
     <Link><Card.Img variant="top" src="https://cdn.wallpapersafari.com/3/57/r2K0SO.jpg"/></Link>
-        <Card.Body className="bkg-blk">
-        </Card.Body>
     </Card>
     </CardDeck>
 </Container>
